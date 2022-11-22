9fd18f3f7a83bb76a74e09636f4657e806908170
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-11-21 21:44:04 -0300

    create Login routes
    definito o redirecionamento da page signIn para a page dashboard caso o usuario esteja logado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { StatusBar } from 'react-native';
 
-import './config/ReactotronConfig';
+import { useSelector } from 'react-redux';
 
-import { store, persistor } from './store';
-import Routes from './routes';
+import createRoutes from './routes';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <Routes />
-      </PersistGate>
-    </Provider>
-  );
+  const signed = useSelector(state => state.auth.signed);
+
+  const Routes = createRoutes(signed);
+
+  return <Routes />;
 }
